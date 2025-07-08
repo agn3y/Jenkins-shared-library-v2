403bf9c5699e4210b8e639545f9a8c91138bc340
agn3y <agneypatel96@gmail.com>
2025-07-09 01:22:49 +0530

    fixed dockerBuild file and structure

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 import com.example.Docker
 
-def call() {
-    def imageName = env.IMAGE_NAME ?: 'agneypatel/test-repoo:latest'
+def call(String imageName) {
+    imageName = imageName ?: env.IMAGE_NAME ?: 'agneypatel/test-repoo:latest'
     return new Docker(this).buildDockerImage(imageName)
 }
 
