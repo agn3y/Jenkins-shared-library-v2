b9f9e7afb2bde12655a08023874039a6c3195163
Agney Patel <85648893+agn3y@users.noreply.github.com>
2025-07-08 14:05:05 +0530

    Delete buildImage.groovy

--- buildImage.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/rnv groovy
-def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-}
