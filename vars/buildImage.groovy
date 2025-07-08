17a202a5b8eacf7bb0440d29576403776e491920
agn3y <agneypatel96@gmail.com>
2025-07-09 00:19:34 +0530

    new commit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 import com.example.Docker
+
 def call() {
+    def imageName = env.IMAGE_NAME ?: 'agneypatel/test-repoo:latest'
     return new Docker(this).buildDockerImage(imageName)
 }
 
