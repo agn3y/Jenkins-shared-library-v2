c9eed2af241972b49615c9eb1ed068358c1b469b
agn3y <agneypatel96@gmail.com>
2025-07-09 00:33:39 +0530

    Fixed class declaration and withCredentials block

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,14 +1,24 @@
-def buildDockerImage(String imageName) {
-    script.echo "Building the docker image"
-    
-    script.withCredentials([script.usernamePassword(
-        credentialsId: 'Dockerhub', 
-        passwordVariable: 'PASS', 
-        usernameVariable: 'USER'
-    )]) {
-        script.sh "docker build -t $imageName ."
-        script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
-        script.sh "docker push $imageName"
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "Building the docker image"
+        script.withCredentials([script.usernamePassword(
+            credentialsId: 'Dockerhub', 
+            passwordVariable: 'PASS', 
+            usernameVariable: 'USER'
+        )]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
+            script.sh "docker push $imageName"
+        }
     }
 }
 
