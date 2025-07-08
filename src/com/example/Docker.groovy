04bdaf94740468693834a47325172ae46f5d4461
agn3y <agneypatel96@gmail.com>
2025-07-09 00:29:42 +0530

    new commit

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,18 +1,14 @@
-#!/usr/bin/env groovy
-package com.example
-
-class Docker implements Serializable{
-
-    def script
-    Docker (script) {
-        this.script = script
-    }
-
-    def buildDockerImage(String imageName) {
-        script.echo "Building the docker image"
-        script.withCredentials([script.usernamePassword(credentialsId: 'Dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')])
+def buildDockerImage(String imageName) {
+    script.echo "Building the docker image"
+    
+    script.withCredentials([script.usernamePassword(
+        credentialsId: 'Dockerhub', 
+        passwordVariable: 'PASS', 
+        usernameVariable: 'USER'
+    )]) {
         script.sh "docker build -t $imageName ."
-        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
         script.sh "docker push $imageName"
     }
 }
+
