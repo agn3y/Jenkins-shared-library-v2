5d3088ce28450c082a0b331b72860a3472e3dcbe
Agney Patel <85648893+agn3y@users.noreply.github.com>
2025-07-08 15:02:42 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,4 @@
-#!/usr/bin/rnv groovy
-def call() {
+def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
